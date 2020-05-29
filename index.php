d32ae77c86124820a7f3ce217230d5cf88269625
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-29 18:38:05 +0200

    corrigé

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <?php
+    // Exercice 2
+    $lastname = 'Mouille';
+    $firstname = 'Erwan';
+    $age = 20;
+    echo 'Je m\'appelle ' . $lastname . ' ' . $firstname . ' et j\'ai ' . $age . ' ans.';
+    ?>
+  </body>
+</html>
